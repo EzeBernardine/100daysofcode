171cca4ee338a134c7067025d95d47c047ed3798
EzeBernardine <demayous2@gmail.com>
2018-08-24 04:12:30 +0100

    var in a for loop

--- let-const.js/let-const.js
+++ let-const.js/let-const.js
@@ -15,8 +15,16 @@ console.log(name)
 var name = 'Ada';
 console.log(name)
 // this will be global. But to restrict it from that, you put it in an IFE
-
+// An IFE is a self invoking function or a self executing anonymous function
 (function(){
   var name = 'Emeka';
-  console.log(name)
+  console.log(name) - Emeka
 })()
+console.log(name) - ''
+// this is coming from the windows
+
+for(var i = 1; i < 10;i++){
+  console.log(i) - 1-10
+}
+i - 10;
+// this is because i has been globally declared
