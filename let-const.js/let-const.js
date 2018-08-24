d6aebbcb71396b4caa359399f9f9c66dd283ed24
EzeBernardine <demayous2@gmail.com>
2018-08-24 04:27:34 +0100

    my last commit for day 2

--- let-const.js/let-const.js
+++ let-const.js/let-const.js
@@ -29,9 +29,16 @@ for(var i = 1; i < 10;i++){
 i - 10;
 // this is because i has been globally declared
 
-for(var i = 1; i < 10;i++){
+for(var i = 1; i < 10; i++){
   setTimeout(function(){
     console.log(`the number is ${i}`) - 10
   }, 1000)
 }
 // setTimeout is a method  that allow your program to run after the sprcified time
+
+// But with let, it gives you the exact value
+for(let i = 1; i < 10; i++){
+  setTimeout(function(){
+    console.log(`the number is ${i}`) - the number is 0-9
+  }, 1000)
+}
