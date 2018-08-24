ac169ddde4a448bdf72c380af2924d6da54474e8
EzeBernardine <demayous2@gmail.com>
2018-08-24 04:03:13 +0100

    intro to IFE in let variables

--- let-const.js/let-const.js
+++ let-const.js/let-const.js
@@ -8,8 +8,15 @@ NAMES()
 // this will say namenot defined
 
 // But if i wrap it in a block,
-{
-  var name = 'Obi'
-}
+{  var name = 'Obi'}
 console.log(name)
 // this is defined and so, not block scoped
+
+var name = 'Ada';
+console.log(name)
+// this will be global. But to restrict it from that, you put it in an IFE
+
+(function(){
+  var name = 'Emeka';
+  console.log(name)
+})()
