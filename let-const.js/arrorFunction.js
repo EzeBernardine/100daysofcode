0c63ceda2b810f3efeeeba578295e6c5a256373d
EzeBernardine <demayous2@gmail.com>
2018-08-25 08:46:45 +0100

    3rd day/ arror function

--- let-const.js/arrorFunction.js
+++ let-const.js/arrorFunction.js
@@ -20,7 +20,35 @@ let names = () => console.log('hi') - 'hi'
 names()
 
 // But for function statement and its a named function
+// its easy to debug
 function names() {
   console.log('hi')
 }
 names()
+
+//
+// To return an object
+let name = (name, age, size)=>{name: name, age: age, size: size};
+name('ada', 2, 32) - error
+
+// But when you wrap it in parenthesis
+let name = (name, age, size)=>({name: name, age: age, size: size});
+name('ada', 2, 32) - Object { name: "ada", age: 2, size: 32 }
+
+// To make things nicer
+let name = (name, age, size)=>({name: name, age: age, size: size});
+console.table(name('ada', 2, 32)) - 
+(index)
+Values
+name
+ada
+age
+2
+size
+32
+// this will apear in a tablular format
+
+// Usind filter
+let age = [2, 2, 5, 8, 6, 8, 2, 4 , 6]
+let fil = age.filter(age => age > 2 );
+console.log(fil) - Array(6)[5, 8, 6, 8, 4, 6]
