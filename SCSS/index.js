3f5fa00ae09609bb9bc12704cc0a8790e4bd7be7
EzeBernardine <demayous2@gmail.com>
2018-12-01 13:18:49 +0100

    editing the input tag

---
+++ SCSS/index.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+  document.getElementsByClassName('input')[0].addEventListener('click', displayName)
+
+ function displayName() {
+  if (document.querySelector('.name') != null) {
+      const name =document.querySelector('.name', '.password').className = 'displayName';
+  }
+ }
+
